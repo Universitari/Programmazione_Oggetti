a417cadfc894d92d80ad81efc21ae9eabf821c8e
Linux-99 <64539251+Linux-99@users.noreply.github.com>
2020-05-29 18:35:15 +0200

    doxy 1.0

--- Acquisto.h
+++ Acquisto.h
@@ -19,16 +19,33 @@ class Acquisto {
     Data DataVendita;
 
     public:
-
+    /// Set ISBN dell'acquisto
+    /// @param tmp variabile_temporanea per ISBN acquisto
     void SetISBN_Acq(string tmp){ ISBN = tmp;};
+    /// Set Tessera del cliente
+    /// @param tmp variabile_temporanea per tessera del cliente
     void SetTessera(unsigned int tmp) {Numero_tessera = tmp;};
+    /// Set Prezzo del libro
+    /// @param tmp variabile_temporanea per prezzo del libro
     void SetPrezzo(float tmp) {Prezzo = tmp;};
+    /// Set Data dell'acquisto
+    /// @param tmp variabile_temporanea per data dell'acquisto
     void SetData(Data tmp) {DataVendita = tmp;};
 
+    /// Get ISBN dell'acquisto
+    /// @returns ISBN acquisto
     string GetISBN_Acq() const {return ISBN;};
+    /// Get Numero tessera del cliente
+    /// @returns tessera del cliente
     unsigned int GetNumero_tessera() const {return Numero_tessera;};
+    /// Get Prezzo del libro
+    /// @returns prezzo del libro
     float GetPrezzo_Acq() const {return Prezzo;};
+    /// Get Data dell'acquisto
+    /// @returns data dell'acquisto
     Data GetData() const {return DataVendita;};
-
+    
+    /// Set Prezzo_Acq
+    /// @param tmp Prezzo_Acq 
     void SetPrezzo_Acq(float tmp);
 };
