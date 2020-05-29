7a293410643b38320cca2e9cc3b6aaa7e89e18e6
Linux-99 <64539251+Linux-99@users.noreply.github.com>
2020-05-29 19:09:18 +0200

    Update Acquisto.h

--- Acquisto.h
+++ Acquisto.h
@@ -23,26 +23,26 @@ class Acquisto {
     /// @param tmp variabile_temporanea per ISBN acquisto
     void SetISBN_Acq(string tmp){ ISBN = tmp;};
     /// Set Tessera del cliente
-    /// @param tmp variabile_temporanea per tessera del cliente
+    /// @param tmp variabile_temporanea per Tessera del cliente
     void SetTessera(unsigned int tmp) {Numero_tessera = tmp;};
     /// Set Prezzo del libro
-    /// @param tmp variabile_temporanea per prezzo del libro
+    /// @param tmp variabile_temporanea per Prezzo del libro
     void SetPrezzo(float tmp) {Prezzo = tmp;};
     /// Set Data dell'acquisto
-    /// @param tmp variabile_temporanea per data dell'acquisto
+    /// @param tmp variabile_temporanea per Data dell'acquisto
     void SetData(Data tmp) {DataVendita = tmp;};
 
     /// Get ISBN dell'acquisto
     /// @returns ISBN acquisto
     string GetISBN_Acq() const {return ISBN;};
     /// Get Numero tessera del cliente
-    /// @returns tessera del cliente
+    /// @returns Numero tessera del cliente
     unsigned int GetNumero_tessera() const {return Numero_tessera;};
     /// Get Prezzo del libro
-    /// @returns prezzo del libro
+    /// @returns Prezzo del libro
     float GetPrezzo_Acq() const {return Prezzo;};
     /// Get Data dell'acquisto
-    /// @returns data dell'acquisto
+    /// @returns Data dell'acquisto
     Data GetData() const {return DataVendita;};
     
     /// Set Prezzo_Acq
