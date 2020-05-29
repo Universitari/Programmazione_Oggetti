5a3ae23168b2bcc5dd3d78ba9772bca239089b4f
Linux-99 <64539251+Linux-99@users.noreply.github.com>
2020-05-29 19:16:26 +0200

    Update Libro.h

--- Libro.h
+++ Libro.h
@@ -15,20 +15,46 @@ class Libro {
 
   public:
 // Funzioni per la gestione delle variabili private
+    /// set Titolo del libro
+    /// @param tmp variabile temporanea del Titolo libro
     void SetTitolo(string tmp) {Titolo = tmp;};
+    /// set Autore del libro
+    /// @param tmp variabile temporanea dell'Autore libro
     void SetAutore(string tmp) {Autore = tmp;};
+    /// set ISBN del libro
+    /// @param tmp variabile temporanea dell'ISBN libro
     void SetISBN(string tmp) {ISBN = tmp;};
+    /// set Prezzo del libro
+    /// @param tmp variabile temporanea del Prezzo libro
     void SetPrezzo(float tmp) {Prezzo = tmp;};
+    /// set Quantità del libro
+    /// @param tmp variabile temporanea della Quantità libro
     void SetQuantita(unsigned int tmp) {Quantita = tmp;};
+    /// set Ordinati del libro
+    /// @param tmp variabile temporanea per gli Ordinati libro
     void SetOrdinati(unsigned int tmp) {Ordinati = tmp;};
-
+    
+    /// Get Titolo del libro
+    /// @returns Titolo libro
     string GetTitolo() const {return Titolo;};
+    /// Get Autore del libro
+    /// @returns Titolo libro
     string GetAutore() const {return Autore;};
-    string GetISBN() const {return ISBN;};
-    float GetPrezzo() const {return Prezzo;};
+    /// Get ISBN del libro
+    /// @returns ISBN libro
+    string GetISBN() const {return ISBN;}; 
+    /// Get Prezzo del libro
+    /// @returns Prezzo libro
+    float GetPrezzo() const {return Prezzo;}; 
+    /// Get Quantità del libro
+    /// @returns Quantità libro
     unsigned int GetQuantita() const {return Quantita;};
+    /// Get Ordinati del libro
+    /// @returns Ordinati libro
     unsigned int GetOrdinati() const {return Ordinati;};
-
+    
+    /// Get Titolo libro
+    /// @returns Titolo libro
     void RiduciQuantita(int tmp) {Quantita = Quantita - tmp;};
 
     Libro& operator=(const Libro &tmp);
